2e04f0126bf616c201ea574acff288fed8059e7c
Gurpreet <gurpreetsingh.panesar1996@gmail.com>
2022-01-19 00:51:17 +0530

    Parameters page added

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ class HomePage(QMainWindow):      #home page
         super(HomePage,self).__init__()
         loadUi("./UI/homePage.ui",self)
         self.btn_diagnostics.clicked.connect(self.gotoDiagnostics)
+        self.btn_parameters.clicked.connect(self.gotoParam)
 
     def gotoDiagnostics(self):
         widget.setCurrentIndex(1)
+
+    def gotoParam(self):
+        widget.setCurrentIndex(3)
 
 
 class DiagnosticsInputsPage(QMainWindow): #diagnostics input page
@@ -43,15 +47,28 @@ class DiagnosticsOutputsPage(QMainWindow): #diagnostics output page
     def gotoDiagIn(self):
         widget.setCurrentIndex(1)
 
+class ParametersPage(QMainWindow): #diagnostics input page
+    def __init__(self):
+        super(ParametersPage,self).__init__()
+        loadUi("./UI/parameters.ui",self)
+        self.btn_back.clicked.connect(self.gotoHome)    #call Back btn fuction
+        scroller = self.findChild(QScrollArea, "scrollArea")
+        QScroller.grabGesture(scroller.viewport(), QScroller.LeftMouseButtonGesture)
+
+    def gotoHome(self):
+        widget.setCurrentIndex(0)
+
 if __name__ == '__main__':
     app=QApplication(sys.argv)
     homePage = HomePage()
     diagInPage = DiagnosticsInputsPage()
     diagOutPage = DiagnosticsOutputsPage()
-    widget=QtWidgets.QStackedWidget()
+    paramPage = ParametersPage()
+    widget = QtWidgets.QStackedWidget()
     widget.addWidget(homePage)
     widget.addWidget(diagInPage)
     widget.addWidget(diagOutPage)
+    widget.addWidget(paramPage)
     widget.setFixedWidth(800)
     widget.setFixedHeight(480)
     widget.show()
